edf27cc5d54111c89cc03f2abc95a72bc08959eb
Qder <p.savickas.93@gmail.com>
2014-03-18 16:45:39 +0200

    pasaass

---
+++ MusicEditor/Controllers/PlayController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicEditor.Controllers
+{
+    public class PlayController : Controller
+    {
+        //
+        // GET: /Play/
+
+        public ActionResult Music()
+        {
+            return View();
+        }
+    [HttpPost]
+        public ActionResult Music()
+        {
+            return;
+        }
+
+    }
+}
---
+++ MusicEditor/Models/PlayMusicViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MusicEditor.Models
+{
+    public class PlayMusicViewModel
+    {
+        public bool IsPlay { get; set; }
+        public long From { get; set; }
+        public long To { get; set; }
+    }
+}
